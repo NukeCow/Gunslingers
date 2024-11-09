e665924cb10280769dcc366a8f17f71cceb2438a
NukeCow <nuclearcow101@gmail.com>
2024-11-08 20:34:58 -0800

    Added Dodging and Sprinting

--- Gunslingers/Assets/Scripts/PMovement.cs
+++ Gunslingers/Assets/Scripts/PMovement.cs
@@ -13,10 +13,13 @@ public class PMovement : MonoBehaviour
 	float turnSmoothVelocity; //A reference variable for turn clamping
 	Vector3 velocity; //The player's velocity. Currently only used for gravity calculations
 	bool isGrounded = false; //Is the player on the ground?
+	Vector3 inputDirection; //The direction of the player's inputs
 
 	public float speedDodge; //How fast the player moves when dodging
 	public float dodgeKeyHoldTime; //Time, in seconds, the player must hold down the dodge key to start sprinting
 	public float speedSprint; //How fast the player moves when sprinting
+	public float dodgeTime; //The length of time, in seconds, the player's dodge is
+	public bool canControl = true; //Are the player's controls enabled?
 	bool isSprinting = false; //Is the player sprinting?
     
     [SerializeField] CharacterController pController; //Reference to the player's Character Controller component
@@ -24,28 +27,25 @@ public class PMovement : MonoBehaviour
 	[SerializeField] Transform groundCheck; //Reference to the ground checker object's transform component
 
 	[SerializeField] KeyCode keyDodge; //The key assigned to the Dodge action
+
 	// Update is called once per frame
 	void Update()
     {
 		isGrounded = Physics.CheckCapsule(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckHeight, groundCheck.position.z), groundCheckRadius, groundMask);
-		if (isGrounded && velocity.y < 0) velocity.y = -2f;
+		if (isGrounded && velocity.y < 0) velocity.y = 0f;
 
-		float _horizontal = Input.GetAxisRaw("Horizontal");
-		float _vertical = Input.GetAxisRaw("Vertical");
-		Vector3 _direction = new Vector3(_horizontal, 0f, _vertical).normalized;
-
-		if (Input.GetKeyDown(keyDodge)) StartCoroutine(DodgeKeyHoldTime(dodgeKeyHoldTime, _direction));
-		if (Input.GetKeyUp(keyDodge)) isSprinting = false;
-
-		if(_direction.magnitude >= 0.1f)
+		if (canControl)
 		{
-			if (isSprinting)
+			float _horizontal = Input.GetAxisRaw("Horizontal");
+			float _vertical = Input.GetAxisRaw("Vertical");
+			inputDirection = new Vector3(_horizontal, 0f, _vertical).normalized;
+			if (Input.GetKeyDown(keyDodge)) StartCoroutine(SprintTest(dodgeKeyHoldTime));
+			if (Input.GetKeyUp(keyDodge)) isSprinting = false;
+
+			if (inputDirection.magnitude >= 0.1f)
 			{
-				pController.Move(pMoveVector(1, _direction, speedSprint));
-			}
-			else
-			{
-				pController.Move(pMoveVector(0, _direction, speed));
+				if (isSprinting) MovePlayer(speedSprint);
+				else MovePlayer(speed);
 			}
 		}
 		if (!isGrounded)
@@ -55,39 +55,47 @@ public class PMovement : MonoBehaviour
 		}
 	}
 	
-	IEnumerator DodgeKeyHoldTime(float _sprintTime, Vector3 _direction)
+	IEnumerator SprintTest(float _sprintTime)
 	{
-		float _time = 0f;
-		while (Input.GetKey(keyDodge) && _time < _sprintTime)
+		float _startTime = Time.time;
+		while (Time.time < _startTime + _sprintTime)
 		{
-			_time += Time.deltaTime;
+			if (Input.GetKeyUp(keyDodge))
+			{
+				StartCoroutine(Dodge());
+				canControl = false;
+				break;
+			}
+			yield return null;
 		}
-
-		if(_time >= _sprintTime)
+		if (Time.time >= _startTime + _sprintTime)
 		{
 			isSprinting = true;
-		}
-		else
-		{
-			pController.Move(pMoveVector(2, _direction, speedDodge));
 		}
 		yield return null;
 	}
 
-	Vector3 pMoveVector(int _moveType, Vector3 _direction, float _speed)
+	IEnumerator Dodge()
 	{
-		/* Player Movement Type IDs
-		 * 0 = Walking
-		 * 1 = Sprinting
-		 * 2 = Dodging
-		 */
-		float _trgtAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + pCam.eulerAngles.y;
+		float _startTime = Time.time;
+		while (Time.time < _startTime + dodgeTime)
+		{
+			MovePlayer(speedDodge);
+			yield return null;
+		}
+		canControl = true;
+		isSprinting = false;
+	}
+
+	void MovePlayer(float _speed)
+	{
+		float _trgtAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + pCam.eulerAngles.y;
 		float _angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, _trgtAngle, ref turnSmoothVelocity, turnSmoothing);
 		transform.rotation = Quaternion.Euler(0f, _angle, 0f);
 
 		Vector3 _faceDir = Quaternion.Euler(0f, _trgtAngle, 0f) * Vector3.forward;
 		Vector3 _move = _faceDir.normalized * _speed * Time.deltaTime;
 
-		return _move;
+		pController.Move(_move);
 	}
 }
